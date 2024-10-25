ded96bdeac8e98fe89e470dde25769b117d4c683
aykhumalo1 <ayanda.khumalo@momentum.co.za>
2024-10-25 09:36:24 +0200

    Second Commit

--- src/main/java/org/chatapp/config/SecurityConfig.java
+++ src/main/java/org/chatapp/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.chatapp.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfiguration {
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
@@ -35,14 +36,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(@NotNull AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
 
 
 
-    protected void configure(HttpSecurity http) throws Exception {
+    
+    protected void configure(@NotNull HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/authenticate").permitAll()  // Allow authentication without JWT
